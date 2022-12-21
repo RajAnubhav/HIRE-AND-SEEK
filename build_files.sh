1bc8db985fc64359df58772e449fc4437e23b269
RajAnubhav <anubhavraj742@gmail.com>
2022-12-22 00:50:48 +0530

    updated build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -4,6 +4,6 @@ python3.10 -m pip install virtualenv
 python3.10 -m virtualenv env
 source env/bin/activate
 
-python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput --clear
+python3.10 -m pip install -r requirements.txt
+python3.10 manage.py collectstatic --noinput --clear
 echo "BUILD END"
