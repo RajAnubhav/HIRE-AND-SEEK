f01a7f3a73db0d9902582fbd237a817c0acde42a
RajAnubhav <anubhavraj742@gmail.com>
2022-12-22 00:05:36 +0530

    adding the styles

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3f=a99*jm)-%%h(=z0edoxvkr(90^v=c+nru$09y^&%&ltpf*p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh', 'localhost', '']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -114,19 +114,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
-# STATIC_URL = 'static/'
-# STATICFILES_DIRS=[
-#     BASE_DIR / "static",
-# ]
-
+import os
+STATIC_URL = 'static/'
+STATICFILES_DIRS=[
+    BASE_DIR / "static",
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-# Static files (CSS, JavaScript, Images)
-import os
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
