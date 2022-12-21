1db7b32664f3a92a2ab027b4d49f22d0766a5818
RajAnubhav <anubhavraj742@gmail.com>
2022-12-22 00:59:20 +0530

    update build_files.sh 1

--- build_files.sh
+++ build_files.sh
@@ -4,7 +4,7 @@ python3 -m pip install virtualenv
 python3 -m virtualenv env
 source env/bin/activate
 
-python3 -m pip install https://dev.mysql.com/doc/refman/8.0/en/macos-installation-pkg.html
+python3 -m pip install mysql-client
 
 python3 -m pip install -r requirements.txt
 python3 manage.py collectstatic --noinput --clear
