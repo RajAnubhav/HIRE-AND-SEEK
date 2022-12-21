244964e99390deab868bd0ef5f59458e91daa9ce
RajAnubhav <anubhavraj742@gmail.com>
2022-12-22 00:48:47 +0530

    python3.9 -> python3.10

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,7 @@
 echo "BUILD START"
-python3.9 -m pip install --upgrade pip
-python3.9 -m pip install virtualenv
-python3.9 -m virtualenv env
+python3.10 -m pip install --upgrade pip
+python3.10 -m pip install virtualenv
+python3.10 -m virtualenv env
 source env/bin/activate
 
 python3.9 -m pip install -r requirements.txt
