2153283eb3d095dd6ba3805b2696ff4177c665e7
RajAnubhav <anubhavraj742@gmail.com>
2023-04-08 01:25:53 +0530

    updated code

--- build_files.sh
+++ build_files.sh
@@ -1,11 +1,8 @@
 echo "BUILD START"
 python3 -m pip install --upgrade pip
-python3 -m pip install virtualenv
-python3 -m virtualenv env
-source env/bin/activate
+python3 -m pip install -r requirements.txt
 
 python3 -m pip install mysql-client
 
-python3 -m pip install -r requirements.txt
 python3 manage.py collectstatic --noinput --clear
 echo "BUILD END"
--- mysite/settings.py
+++ mysite/settings.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -61,12 +61,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
@@ -107,6 +107,12 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS=[
     BASE_DIR / "static",
 ]
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+MEDIA_URLS ='/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
